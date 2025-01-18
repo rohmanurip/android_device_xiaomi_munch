35c1b7a348819a96881e65ce3adf1a532f40a0b1
LuK1337 <priv.luk@gmail.com>
2025-01-18 18:33:32 +0100

    munch: Move WfdCommon boot jar declaration to vendor
    Change-Id: I8fa2c0f64692873b432cc252ec88b388a32023a1

--- device.mk
+++ device.mk
@@ -459,8 +459,5 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libwfdaac_vendor
 
-PRODUCT_BOOT_JARS += \
-    WfdCommon
-
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/munch/munch-vendor.mk)
