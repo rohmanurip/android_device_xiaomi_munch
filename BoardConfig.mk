c1dccead741d6e3d1e36bb4f523872367eb4649d
dkpost3 <dkpost3@@users.noreply.github.com>
2025-04-27 21:26:29 +0000

    munch: fixing connecting BLE (bt 5.x) devices (smart watches, etc.)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -59,6 +59,9 @@ WITH_DEXPREOPT := true
 DEX_PREOPT_DEFAULT := generate-vdex-and-image
 WITH_DEXPREOPT_DEBUG_INFO := false
 
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/configs/bluetooth/include
+
 # Board
 TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/configs/board-info.txt
 
