0d3f1b71fef7f2400ff428796f77ef0b16634d27
HDzungx <hdzungx@gmail.com>
2025-06-20 07:28:35 +0000

    munch: Shim libwfdservice with libaudioclient_shim

--- extract-files.py
+++ extract-files.py
@@ -50,7 +50,8 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libbinder_shim.so')
         .add_needed('libinput_shim.so'),
     'system_ext/lib64/libwfdservice.so': blob_fixup()
-        .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
+        .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so')
+        .add_needed('libaudioclient_shim.so'),
     'vendor/etc/init/init.batterysecret.rc': blob_fixup()
         .regex_replace(' +seclabel u:r:batterysecret:s0\n', ''),
     'vendor/etc/init/init.mi_thermald.rc': blob_fixup()
