636a5084160746df4ff8955b856ddefca8275971
HDzungx <hdzungx@gmail.com>
2025-07-17 19:14:46 +0700

    munch: Remove fastcharge

--- device.mk
+++ device.mk
@@ -187,10 +187,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
-# Fastcharge
-PRODUCT_PACKAGES += \
-    vendor.lineage.fastcharge@1.0-service.xiaomi_munch
-
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint-service.xiaomi
