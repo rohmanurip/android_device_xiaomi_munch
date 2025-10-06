0dde22524acd536a9fca3d6e988305919ae03b76
SenseiiX <senseixdev@gmail.com>
2025-10-06 19:19:07 +0800

    munch: Remove more packages
    Signed-off-by: SenX <senseiix@dev.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Chrome-Stub ViaBrowser Chrome64 YouTube
+LOCAL_OVERRIDES_PACKAGES := Chrome-Stub ViaBrowser Chrome64 YouTube Accord MusicFX
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
