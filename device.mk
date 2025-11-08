49649b4a6dbfeda0dfec1c7b7c6a9bfa319db6a8
Kenaidi01 <andrianp920@gmail.com>
2025-11-08 19:55:27 +0700

    munch: build GameBar
    Signed-off-by: Kenaidi01 <andrianp920@gmail.com>

--- device.mk
+++ device.mk
@@ -19,6 +19,9 @@ ifeq ($(TARGET_USES_DOLBY),true)
 $(call inherit-product, hardware/dolby/dolby.mk)
 endif
 
+# GameBar Performance Overlay
+$(call inherit-product, packages/apps/GameBar/gamebar.mk)
+
 # Private key for signed build
 -include vendor/private-keys/keys/keys.mk
 
