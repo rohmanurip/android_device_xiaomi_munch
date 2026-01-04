232d9d1a7e8f6efc60a1fbd9f8e0ae23ffea4ef6
RohmanU <67408676+rohmanurip@users.noreply.github.com>
2026-01-05 05:40:10 +0700

    Change config camera st_license.lic to secret

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,3 +2,5 @@ cd kernel/xiaomi/munch
 chmod +x nextpatch.sh && bash nextpatch.sh
 rm -rf KernelSU/userspace/su
 cd ../../..
+
+base64 -d device/xiaomi/munch/configs/camera/secret > device/xiaomi/munch/configs/camera/st_license.lic
